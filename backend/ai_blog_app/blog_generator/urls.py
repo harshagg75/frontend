bb4476604c90eca5a1fd0c7072564334c47b4d74
ADMIN <harshagg75@gmail.com>
2024-07-29 09:50:56 +0530

    add the authentication

--- backend/ai_blog_app/blog_generator/urls.py
+++ backend/ai_blog_app/blog_generator/urls.py
@@ -4,5 +4,9 @@ from .import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index ,name='index')
+    path('', views.index ,name='index'),
+    path('login', views.user_login ,name='login'),
+    path('signup', views.user_signup ,name='signup'),
+    path('logout', views.user_logout ,name='logout'),
+
 ]
